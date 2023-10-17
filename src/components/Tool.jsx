2b4cbc812006748f4d5daac7d91a4eaef85118d3
MixelinSoft <mixelinsoft@gmail.com>
2023-10-17 16:53:34 +0300

    Added Info on click Tool Title

--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -1,15 +1,31 @@
 import styles from "./Tool.module.css";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 import tools from "../utils/tools";
+import ModalInfo from "./UI/ModalInfo";
 
 const Tool = () => {
   const toolSlug = useParams().slug.toString();
   const selectedTool = tools.find((tool) => toolSlug === tool.toolSlug);
+  const [modalShowState, setModalShowState] = useState(false);
+  const setModalShowHandler = (show) => {
+    setModalShowState(show);
+  };
+
   return (
     <Container className={styles.toolContainer}>
-      <h2>{selectedTool.toolTitle}</h2>
-      <p>v{selectedTool.toolVersion}</p>
+      <ModalInfo
+        show={modalShowState}
+        showToggler={setModalShowState}
+        headerText={selectedTool.toolTitle + " v" + selectedTool.toolVersion}
+        bodyText={selectedTool.toolDescription}
+      />
+      <h2 onClick={() => setModalShowHandler(!modalShowState)}>
+        {selectedTool.toolTitle}
+      </h2>
+
       {selectedTool.tool()}
     </Container>
   );
