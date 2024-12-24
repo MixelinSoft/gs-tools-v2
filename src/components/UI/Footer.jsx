e7a5ce2c351c91093152604dca95de38ddc35915
MixelinSoft <127510809+MixelinSoft@users.noreply.github.com>
2024-12-24 11:03:21 +0200

    Report Generator - Update footer to display the current year dynamically

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -2,9 +2,11 @@ import styles from './Footer.module.css';
 import Navbar from 'react-bootstrap/Navbar';
 
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
   return (
     <Navbar className={`${styles.footer} bg-light notranslate`} translate='no'>
-      ©MixelinSoft 2023-2024
+      ©MixelinSoft 2023-{year}
     </Navbar>
   );
 };
