8470a44f3894d1876785abc42758529773fad3f2
MixelinSoft <127510809+MixelinSoft@users.noreply.github.com>
2024-12-24 11:13:35 +0200

    Bump version to 3.11.5 and update PhoneBook tool version to 1.4.3

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -88,7 +88,7 @@ const tools = [
 
     toolID: '04',
     toolSlug: 'phone-book',
-    toolVersion: '1.4.2',
+    toolVersion: '1.4.3',
     tool: PhoneBook,
     icon: <MdOutlineContactPhone />,
   },
